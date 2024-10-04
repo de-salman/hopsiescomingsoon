b696947ddf927d0ed886966efbf0e792dfa3f744
de-salman <salmanfarisi.bca@gmail.com>
2024-10-04 17:54:39 +0530

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@
 export default function Home() {
   return (
     <div>
-      <img src="https://res.cloudinary.com/dfznv3tjy/image/upload/f_auto,q_auto/v1728042380/Hopsies/dfevkwqy9tqziixu2ytv.png" width={"100%"} />
+      <img className="desktop" src="https://res.cloudinary.com/dfznv3tjy/image/upload/f_auto,q_auto/v1728042380/Hopsies/dfevkwqy9tqziixu2ytv.png" width={"100%"} />
+      <img className="mobile" src="https://res.cloudinary.com/dfznv3tjy/image/upload/v1728044558/na6o3ayqvc4gvnadswia.png" width={"100%"} />
     </div>
   );
 }
