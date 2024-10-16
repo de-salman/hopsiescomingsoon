ce4ddaf965defb35f675d37a0d1e8be9eb463c97
de-salman <salmanfarisi.bca@gmail.com>
2024-10-16 16:42:46 +0530

    mobile video autoplay 5

--- src/app/page.js
+++ src/app/page.js
@@ -58,7 +58,7 @@ export default function Home() {
           cursor: "pointer",
         }}
       >
-        {isMuted ? (
+        {!isMuted ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
